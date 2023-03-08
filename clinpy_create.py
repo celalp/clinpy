208d5afff54a8c41c7cc6e5296b0e37ed92bb69a
LaurenL <lyj95618@gmail.com>
2023-03-08 16:40:15 -0500

    first version, should be working with my own config.py

--- clinpy_create.py
+++ clinpy_create.py
@@ -14,20 +14,40 @@ import yaml
 import sys
 from config import *
 
+import pandas as pd
+import openpyxl
+
 from clinpy.utils.utils import dict_to_table, dict_to_engine
 from clinpy import  project
 
 from sqlalchemy import MetaData
 from sqlalchemy.orm import Session
 
-parser = arg.ArgumentParser(description='add to a project database with genome annotations junctions'
+if __name__ == "__main__":
+    parser = arg.ArgumentParser(description='add to a project database with genome annotations junctions'
                                             'and expression data')
-parser.add_argument('-c', '--config', help="config.py file see readme for details", type=str, action="store",
+    parser.add_argument('-c', '--config', help="config.py file see readme for details", type=str, action="store",
                     default="config.py")
-args = parser.parse_args()
-
-#TODO import config
-
-db=dict_to_engine(database)
-
-for assay in data.keys()
+    args = parser.parse_args()
+    
+    #TODO import config
+    exec(open("config.py").read())
+    
+    db=dict_to_engine(database['dbtype'],database['name'])
+    project=Project(db_params=database)
+    
+    create=True#should be one of the arguments in the input
+    
+    #temp setting
+    for assay in data.keys():
+        if assay == 'base':
+        print('starting base')
+        data[assay]['assay'].import_data(data[assay]['filename'], project=project,
+                                       read_fun=pd.read_excel,read_fun_params={},
+                                      assay_params=data[assay]['assay_params'], create_assay=create)
+        else:
+        print(assay)
+        data[assay]['assay'].import_data(data[assay]['filename'], project=project,
+                                       meta_read_fun=pd.read_excel,
+                                       read_fun=data[assay]['read_fun'],
+                                      assay_params=data[assay]['assay_params'], create_assay=create)
